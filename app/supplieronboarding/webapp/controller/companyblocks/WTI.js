28934bd6e7d6483e1aa57bad7c5e6d3b481bfd3a
Gunjan Sharma <gunjan.designer25@gmail.com>
2024-10-16 10:30:06 +0000

    company code data

---
+++ app/supplieronboarding/webapp/controller/companyblocks/WTI.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    'sap/uxap/BlockBase'
+], function (BlockBase) {
+    "use strict";
+
+    return BlockBase.extend("com.sumo.supplieronboarding.view.companyblocks.WTI", {
+        metadata: {
+            /* no additional views provided */
+        }
+      
+    });
+});
